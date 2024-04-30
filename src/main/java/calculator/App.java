e49f0799fbd0b365b4c9aaad0bac79d04a7cca0f
yoon <pyh051920@gmail.com>
2024-04-30 21:05:27 +0900

    Level 1 - 1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,6 +12,6 @@ public class App {
         int num2 = sc.nextInt();
         // Scanner 를 사용하여 양의 정수를 입력 받고 적합한 타입의 변수에 저장
 
-        System.out.println("num1, num2 = " + num1 + ", " + num2);
+        System.out.println("num1, num2 = " + num1 + " , " + num2);
     }
 }
