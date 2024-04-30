9f8a8177939d8d9901108628e30c5a430d00df94
Robbie <robbie331@>
2024-04-30 20:50:51 +0900

    Level 1-1 num1,2 added

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,17 @@
 package calculator;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("type first num");
+        int num1 = sc.nextInt();
+        System.out.println("type second num");
+        int num2 = sc.nextInt();
+        // Scanner 를 사용하여 양의 정수를 입력 받고 적합한 타입의 변수에 저장
+
+        System.out.println("num1, num2 = " + num1 + ", " + num2);
     }
 }
