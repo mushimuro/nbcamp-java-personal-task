896c9480cd986954e9d087ac58f48500fc1f6f87
yoon <pyh051920@gmail.com>
2024-04-30 21:15:26 +0900

    Level 1 - 2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,12 +6,13 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("type first num");
+//        System.out.println("type first num");
         int num1 = sc.nextInt();
-        System.out.println("type second num");
+//        System.out.println("type second num");
         int num2 = sc.nextInt();
         // Scanner 를 사용하여 양의 정수를 입력 받고 적합한 타입의 변수에 저장
 
-        System.out.println("num1, num2 = " + num1 + " , " + num2);
+//        System.out.println("num1, num2 = " + num1 + " , " + num2);
+        char operator= sc.next().charAt(0);
     }
 }
