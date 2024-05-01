f1e36033867d3feead6ff0072efdd37f3a0a8b0e
yoon <pyh051920@gmail.com>
2024-05-01 11:30:20 +0900

    Level 2 - 2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,47 +5,26 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) throws Exception{
+        Calculator calculator = new Calculator();
         Scanner sc = new Scanner(System.in);
         boolean loopingCheck = true;
+        int answer;
+
         ArrayList<Integer> answerList = new ArrayList<>();
-//        int count = 0;  //answer 갯수 확인용
 
         while(loopingCheck) {
             System.out.println("type first num");
-            int num1 = sc.nextInt();
+            String num1 = sc.nextLine();
             System.out.println("type second num");
-            int num2 = sc.nextInt();
-            // Scanner 를 사용하여 양의 정수를 입력 받고 적합한 타입의 변수에 저장
-
+            String num2 = sc.nextLine();
             System.out.println("type in operator");
-            char operator = sc.next().charAt(0);
-            int result = 0;
-            // 다음 입력값을 받기 위해 사용
-            String removingEmptySpace = sc.nextLine();
+            String operator = sc.nextLine();
 
-            // 사칙연산에 따른 연산
-            try {
-                if (operator == '+') {
-                    result = num1 + num2;
-                } else if (operator == '-') {
-                    result = num1 - num2;
-                } else if (operator == '*') {
-                    result = num1 * num2;
-                } else if (operator == '/') {
-                    // 0 으로 나눌시 예외 처리
-                    if (num2 == 0) {
-                        throw new Exception("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    } else {
-                        result = num1 / num2;
-                    }
-                }
-                System.out.println("결과: " + num1 + " " + operator + " " + num2 + " = " + result);
-                answerList.add(result);
-            } catch(Exception e){
-                System.out.println(e.getMessage());
-            }
-
-//            System.out.println(answerList);
+            int firstNum = Integer.parseInt(num1);
+            int secondNum = Integer.parseInt(num2);
+            char operatorChar = operator.charAt(0);
+            answer = calculator.calculate(firstNum, secondNum, operatorChar);
+            answerList.add(answer);
 
             // 첫번째값 삭제할지 선택
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,45 @@
 package calculator;
 
-public class Calculator {
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.Scanner;
 
+public class Calculator {
+    ArrayList<Integer> answerList = new ArrayList<>();
+    private int firstNum;
+    private int secondNum;
+    private char operatorUsing;
+    private int result;
+    private static final String OPERATION_REG = "[+\\-*/]";
+    private static final String NUMBER_REG = "^[0-9]*$";
+
+    public Calculator() {
+    }
+
+    public Integer calculate(int firstNum, int secondNum, char operatorUsing) throws Exception{
+        try {
+            if (operatorUsing == '+') {
+                result = firstNum + secondNum;
+            } else if (operatorUsing == '-') {
+                result = firstNum - secondNum;
+            } else if (operatorUsing == '*') {
+                result = firstNum * secondNum;
+            } else if (operatorUsing == '/') {
+                // 0 으로 나눌시 예외 처리
+                if (secondNum == 0) {
+                    throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                } else {
+                    result = firstNum / secondNum;
+                }
+            } else{
+                throw new ArithmeticException("operator 를 잘못 입력했습니다.");
+            }
+            System.out.println("결과: " + firstNum + " " + operatorUsing + " " + secondNum + " = " + result);
+            answerList.add(result);
+            System.out.println(answerList);
+        } catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
 }
